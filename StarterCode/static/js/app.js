21452f833fc3f758e3f38e8d5eaf417bede10388
andyhyshi <57511290+andyhyshi@users.noreply.github.com>
2020-05-24 10:00:11 -0500

    done

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,138 +1,95 @@
-// Use d3.json() to fetch data from JSON file
-var belly_data = "samples.json"
-
-function panelFunction(d) {
+// build function to get the metadata for demographic table
+function getMeta(samples) {
   d3.json("samples.json").then((data) => {
-      var firstData = data.metadata;
+      var mData = data.metadata;
 
-      var id_dropdown = firstData.filter(sampleID => sampleID.id == d);
-      var IDNum = id_dropdown[0];
+      var idFilter = mData.filter(sampleID => sampleID.id == samples);
+      var IDnumber = idFilter[0];
 
-      var metadisplay = d3.select("#sample-metadata");
-      metadisplay.html("");
+      var panelData = d3.select("#sample-metadata");
+      panelData.html("");
 
-      Object.entries(IDNum).forEach(([key, value]) => {
-        metadisplay.append("h6").text(`${key}: ${value}`);
+      Object.entries(IDnumber).forEach(([key, value]) => {
+          panelData.append("h6").text(`${key.toUpperCase()}: ${value}`);
       });
   });
 };
 
-function getPlot(id) {
-  d3.json(belly_data).then((data)=> {
-           var data = data.samples;
-
-      var id_data  = data.filter(sampleID => sampleID.id == id);
-          
-      var id_names = []     
-
-    function appendIDs(otus) {
-        var d = []
-        for (var i = 0; i < otus.length; i++) {
-          d.push(otus[i].id);};
-      return d[0];              
-      };
-    
-      function appendOtu(otus) {
-        var e = []
-        e_sliced = []
-        for (var i = 0; i < otus.length; i++) {
-          e.push(otus[i].otu_ids);};
-
-        for (var i = 0; i < e.length; i++) {
-          e_sliced.push(e[i].slice(0, 10).reverse());}; 
-
-      return e_sliced[0];              
-      };
-     
-
-      function appendValues(otus) {
-        var f = []
-        f_sliced = []
-        for (var i = 0; i < otus.length; i++) {
-          f.push(otus[i].sample_values);};
-
-        for (var i = 0; i < f.length; i++) {
-          f_sliced.push(f[i].slice(0, 10).reverse());};   
-      return f_sliced[0];              
-      };
-
-      id_names = appendIDs(id_data);
-
-      otu_list = appendOtu(id_data);
-
-      values_list = appendValues(id_data);
-
-      otu_string = otu_list.toString().split(",")
-    
-      console.log(id_names);
-      console.log(otu_string);   
-      console.log(values_list);
-
+// plot the charts
+function bubbleData(samples) {
+  d3.json("samples.json").then((data) => {
+      var bubSample = data.samples;
       
- //Bar Chart
+      var idFilter = bubSample.filter(sampleID => sampleID.id == samples);
+      var IDnum = idFilter[0];
 
- var ylabelbar = otu_string.map(otuID => `OTU ${otuID}`)
+      var otuids = IDnum.otu_ids;
+      var otulabels = IDnum.otu_labels;
+      var sampleVals = IDnum.sample_values;
 
+      var bubbleLayout = {
+          title: "Bacteria Cultures Per Sample",
+          margin: { t: 0},
+          hovermode: "closest",
+          xaxis:  {title: "OTU ID" },
+          margin: { t: 30 }
+      };
+      var bubbleData = [{
+          x: otuids,
+          y: sampleVals,
+          text: otulabels,
+          mode: "markers",
+          marker: {
+              size: sampleVals,
+              color: otuids,
+              colorscale: "Portland"
+          }
+      }];
+      
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+      
+      var yticks = otuids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+      var barData = [{
+          y: yticks,
+          x: sampleVals.slice(0, 10).reverse(),
+          text: otulabels.slice(0, 10).reverse(),
+          type: "bar",
+          orientation: "h",
+      }];
+      var barLayout = {
+          title: "Top 10 Bacteria Cultures Found",
+          margin: {
+              t: 30,
+              l: 150
+          }
+      };
+      Plotly.newPlot("bar", barData, barLayout);
 
-var tracebar= [{
-  y: ylabelbar,
-  x: values_list,
-  text: otu_string,
-  type: "bar",
-  orientation: "h",
- }];
-
-var barlayout = {
-  title: `OTUs for ${id_names}`,
-  xaxis: {title: "Count of OTU"},
-  yaxis: {tickmode: "auto",tick0: 0.5,dtick: 0.75}
+  });
 };
 
-Plotly.newPlot("bar", tracebar, barlayout);
-
-//bubble
-
-var xlabelbubble = otu_string.map(otuID => `OTU ${otuID}`)
-
-var bubbletrace = {
-  type: "scatter",
-  mode: "markers",
-  x: xlabelbubble,
-  y: values_list,
-  marker: {
-    size: values_list,
-    text : xlabelbubble    
-     },
-     showlegend: true
-};
-
-var bubdata = [bubbletrace];
- 
-var bublayout = {
-  title: `Count of OTUs`
-};
-
-Plotly.plot("bubble", bubdata, bublayout);
-    
+function init() {
+  // grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+  // use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+          selector
+              .append("option")
+              .text(sample)
+              .property("value", sample);
       });
-    };
+      var firstSample = sampleNames[0];
+      bubbleData(firstSample);
+      getMeta(firstSample);
+  });
+}
 
-    function init() {
-        var selector = d3.select("#selDataset");
-     d3.json("samples.json").then((belly_data) => {
-          var IDDropDown = belly_data.names;
-          IDDropDown.forEach((s) => {
-              selector
-                  .append("option")
-                  .text(s)
-                  .property("value", s);
-          });       
-      });
-  } 
-  
-  function optionChanged(x) {
-           panelFunction(x);
-           getPlot(x);
-  }
-
-  init();
+// build the function to grab new data each time a new sample is selected
+function optionChanged(newSample) {
+  bubbleData(newSample);
+  getMeta(newSample);
+}
+// initialize dashboard
+init();
